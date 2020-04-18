58dcb814427ab319f83d96df75e08e90053a5949
Bryan Borgeson <sonofborge@protonmail.com>
2020-04-18 16:16:47 -0700

    remove all instance of user model and database

--- app/controllers/users_controller.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-# Users controller
-class UsersController < ApplicationController
-  get '/sign-in' do
-    erb :'/users/sign_in'
-  end
-end
--- spec/factories/users.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-FactoryBot.define do
-  factory :user do
-    name { Faker::TvShows::GameOfThrones.character }
-    email { Faker::Internet.email }
-  end
-end
